4cf300eda51c59735f6b2b3bde3d16bda94c0aed
Rizki Lazuardi <rizkilazuardinew90@gmail.com>
2026-09-13 19:17:55 +0700

    Implement basic MVC structure

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MemberController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route for books
+Route::get('/books', [BookController::class, 'index']);
+
+// route for categories
+Route::get('/categories', [CategoryController::class, 'index']);
+
+// route for members
+Route::get('/members', [MemberController::class, 'index']);
